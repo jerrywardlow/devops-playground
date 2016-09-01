8af71f93253b8f5b0af461598f6ca9cc12ae2f98
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-08-31 22:02:36 -0700

    Initial commit

---
+++ terraform-docker/variables.tf
@@ -0,0 +1,39 @@
+/* Skipping these for git reasons and using environment variables
+
+variable "access_key" {
+    description = "AWS access key"
+}
+
+variable "secret_key" {
+    description = "AWS secret access key"
+}
+
+variable "region" {
+    description = "AWS region"
+    default     = "us-west-2"
+}
+*/
+
+variable "vpc_cidr" {
+    description = "/16 CIDR for VPC"
+    default     = "10.128.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+    description = "CIDR for public subnet"
+    default     = "10.128.0.0/24"
+}
+
+variable "private_subnet_cidr" {
+    description = "CIDR for private subnet"
+    default     = "10.128.1.0/24"
+}
+
+variable "amis" {
+    description = "Ubuntu base AMI by region"
+    default = {
+        us-east-1 = "ami-81365496"
+        us-west-1 = "ami-05105d65"
+        us-west-2 = "ami-9ece19fe"
+    }
+}

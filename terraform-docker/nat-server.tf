5fbb9ad94d5a979edeeca4d0870a35435e31c98c
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-01 13:54:03 -0700

    Add user ubuntu to docker group

--- terraform-docker/nat-server.tf
+++ terraform-docker/nat-server.tf
@@ -18,6 +18,7 @@ resource "aws_instance" "nat" {
             "sudo iptables -t nat -A POSTROUTING -j MASQUERADE",
             "echo 1 | sudo tee /proc/sys/net/ipv4/conf/all/forwarding > /dev/null",
             "curl -sSL https://get.docker.com/ | sudo sh",
+            "usermod -G docker ubuntu",
             "sudo mkdir -p /etc/openvpn",
             "sudo docker run --name ovpn-data -v /etc/openvpn busybox",
             "sudo docker run --volumes-from ovpn-data --rm gosuri/openvpn ovpn_genconfig -p ${var.vpc_cidr} -u udp://${aws_instance.nat.public_ip}"

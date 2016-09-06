783fb4e520037f21a825be4cb3751ac4bc50c452
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-05 21:28:01 -0700

    Log connection.remoteAddress

--- gke-kubernetes/server.js
+++ gke-kubernetes/server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
     console.log('Received request for URL: ' + request.url);
+    console.log('Request sent from: ' + request.connection.remoteAddress);
     response.writeHead(200);
     response.end('Hello World!');
 };

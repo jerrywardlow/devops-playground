0b6c2db232da9c495ef5432910cd9b18b32220c9
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-06 18:12:24 -0700

    Security group for MySQL

--- wordpress-cloud/security-groups.tf
+++ wordpress-cloud/security-groups.tf
@@ -105,3 +105,28 @@ resource "aws_security_group" "redis" {
         Name = "wordpress-redis"
     }
 }
+
+# MySQL security group
+resource "aws_security_group" "mysql" {
+    name = "wordpress-mysql"
+    description = "Security group allowing port 3306 from web_staging and web_production"
+    vpc_id = "${aws_vpc.default.id}"
+
+    ingress {
+        from_port = 3306
+        to_port = 3306
+        protocol = "tcp"
+        security_groups = ["${aws_security_group.web_staging.id}", "${aws_security_group.web_production.id}"]
+    }
+
+    egress {
+        from_port = 3306
+        to_port = 3306
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags {
+        Name = "wordpress-mysql"
+    }
+}

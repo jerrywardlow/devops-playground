ad9c2238f1a0d2334f72b86d8ba2ceecd3370284
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-07 16:44:21 -0700

    Production web servers

---
+++ wordpress-cloud/web-production.tf
@@ -0,0 +1,12 @@
+# Production web servers
+resource "aws_instance" "web-production" {
+    count = 2
+    ami = "${var.ubuntu-ami}"
+    instance_type = "t2.micro"
+    subnet_id = "${aws_subnet.default.id}"
+    security_groups = ["${aws_security_group.web_production.id}"]
+    key_name = "${aws_key_pair.wordpress.key_name}"
+    tags = {
+        Name = "wordpress-web-production-${count.index}"
+    }
+}

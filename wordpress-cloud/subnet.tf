369ddc24b5a988547b569f9c8001adfa78e08060
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-06 19:14:56 -0700

    Remove routing table defs

--- wordpress-cloud/subnet.tf
+++ wordpress-cloud/subnet.tf
@@ -14,20 +14,3 @@ resource "aws_subnet" "default" {
         Name = "default"
     }
 }
-
-# Routing table for public subnet
-resource "aws_route_table" "default" {
-    vpc_id = "${aws_vpc.default.id}"
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.default.id}"
-    }
-    tags {
-        Name = "default"
-    }
-}
-
-resource "aws_route_table_association" "default" {
-    subnet_id = "${aws_subnet.default.id}"
-    route_table_id = "${aws_route_table.default.id}"
-}

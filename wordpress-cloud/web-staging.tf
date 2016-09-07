6d17c9d745ab8652a9b91b54778358d05f6208c9
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-06 19:40:27 -0700

    AWS Instance web-staging

---
+++ wordpress-cloud/web-staging.tf
@@ -0,0 +1,12 @@
+# Staging server
+resource "aws_instance" "web-staging" {
+    ami = "${var.ubuntu-ami}"
+    instance_type = "t2.micro"
+    subnet_id = "${aws_subnet.default.id}"
+    security_groups = ["${aws_security_group.web_staging.id}"]
+    key_name = "${aws_key_pair.wordpress.key_name}"
+    source_dest_check = false
+    tags = {
+        Name = "wordpress-web-staging"
+    }
+}

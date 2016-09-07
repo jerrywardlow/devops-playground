da3e117438393774235c6b7b825178d70cf65673
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-06 19:27:56 -0700

    Add Ubuntu AMI

--- wordpress-cloud/variables.tf
+++ wordpress-cloud/variables.tf
@@ -2,3 +2,8 @@ variable "region" {
     description = "AWS region"
     default = "us-west-2"
 }
+
+variable "ubuntu-ami" {
+    description = "Ubuntu AMI for us-west-2"
+    default = "ami-9ece19fe"
+}

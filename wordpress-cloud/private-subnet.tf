3e2a7f37988178b4866d1be30141f73c36ed835a
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-08 14:18:28 -0700

    Update with NAT Gateway ID and typo fix

--- wordpress-cloud/private-subnet.tf
+++ wordpress-cloud/private-subnet.tf
@@ -14,12 +14,12 @@ resource "aws_route_table" "private" {
     vpc_id = "${aws_vpc.default.id}"
     route {
         cidr_block = "0.0.0.0/0"
-        nat_gateway_id = ""
+        nat_gateway_id = "${aws_nat_gateway.nat.id}"
     }
 }
 
 # Routing table association
 resource "aws_route_table_association" "private" {
-    subnet_id = "${aws_subnet.public.id}"
+    subnet_id = "${aws_subnet.private.id}"
     route_table_id = "${aws_route_table.private.id}"
 }

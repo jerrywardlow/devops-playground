236fb62a7d103c528606e40de7652bc3934dc3da
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-08 14:07:06 -0700

    Typos

--- wordpress-cloud/private-subnet.tf
+++ wordpress-cloud/private-subnet.tf
@@ -1,8 +1,8 @@
 # Private subnet
 resource "aws_subnet" "private" {
-    vpc_if = "${aws_vpc.default.id}"
+    vpc_id = "${aws_vpc.default.id}"
     cidr_block = "10.10.9.0/24"
-    avilability_zone = "us-west-2a"
+    availability_zone = "us-west-2a"
     map_public_ip_on_launch = false
     tags {
         Name = "private"

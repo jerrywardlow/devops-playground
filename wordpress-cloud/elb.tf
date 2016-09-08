73d980c9b0b29ae533141f7d6d12bcd4fe2e9a4a
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-07 21:19:08 -0700

    Elastic load balancer

---
+++ wordpress-cloud/elb.tf
@@ -0,0 +1,13 @@
+# Load balancer
+resource "aws_elb" "app" {
+    name = "wordpress-elb"
+    subnets = ["${aws_subnet.default.id}"]
+    security_groups = ["${aws_security_group.elb.id}"]
+    listener {
+        instance_port = 80
+        instance_protocol = "http"
+        lb_port = 80
+        lb_protocol = "http"
+    }
+    instances = ["${aws_instance.web-production.*.id}"]
+}

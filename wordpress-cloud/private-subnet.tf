241a36596240c1cfe4be354651e3d37cfe74a840
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-08 14:11:26 -0700

    Add routing table and association

--- wordpress-cloud/private-subnet.tf
+++ wordpress-cloud/private-subnet.tf
@@ -8,3 +8,18 @@ resource "aws_subnet" "private" {
         Name = "private"
     }
 }
+
+# Routing table
+resource "aws_route_table" "private" {
+    vpc_id = "${aws_vpc.default.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = ""
+    }
+}
+
+# Routing table association
+resource "aws_route_table_association" "private" {
+    subnet_id = "${aws_subnet.public.id}"
+    route_table_id = "${aws_route_table.private.id}"
+}

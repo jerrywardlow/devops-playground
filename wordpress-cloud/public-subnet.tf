115e1f55afbb61e03dd6dc896548a024132b45c3
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-08 14:03:00 -0700

    Add routing table

--- wordpress-cloud/public-subnet.tf
+++ wordpress-cloud/public-subnet.tf
@@ -14,3 +14,12 @@ resource "aws_subnet" "public" {
         Name = "default"
     }
 }
+
+# Routing table
+resource "aws_route_table" "public" {
+    vpc_id = "${aws_vpc.default.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.default.id}"
+    }
+}

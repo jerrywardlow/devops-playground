d53b34c7d204356c8f1d191814075332e03876cc
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-15 13:16:02 -0700

    Add 'nat-ami' variable

--- wordpress-cloud/variables.tf
+++ wordpress-cloud/variables.tf
@@ -7,3 +7,8 @@ variable "ubuntu-ami" {
     description = "Ubuntu 14.04 AMI for us-west-2"
     default = "ami-d732f0b7"
 }
+
+variable "nat-ami" {
+    description = "Amazon Linux AMI VPC NAT x86_64 HVM GP2"
+    default = "ami-69ae8259"
+}

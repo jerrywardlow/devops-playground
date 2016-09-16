6b863b45c817280028440847c275a9853f0c12c9
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-15 17:07:23 -0700

    Change egress to ELB group

--- wordpress-cloud/security-groups.tf
+++ wordpress-cloud/security-groups.tf
@@ -111,7 +111,7 @@ resource "aws_security_group" "web_production" {
         from_port = 0
         to_port = 0
         protocol = -1
-        cidr_blocks = ["0.0.0.0/0"]
+        security_groups = ["${aws_security_group.elb.id}"]
     }
 
     tags {

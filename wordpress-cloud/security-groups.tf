8eb6cad3bf045547cd318775afba214a0e52e8b8
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-16 15:59:44 -0700

    Move wed_production egress to /0 cidr to resolve cycle error

--- wordpress-cloud/security-groups.tf
+++ wordpress-cloud/security-groups.tf
@@ -111,10 +111,8 @@ resource "aws_security_group" "web_production" {
         from_port = 0
         to_port = 0
         protocol = -1
-        security_groups = ["${aws_security_group.elb.id}",
-                           "${aws_security_group.nat.id}",
-                           "${aws_security_group.redis.id}",
-                           "${aws_security_group.mysql.id}"]
+        # security_groups = ["${aws_security_group.elb.id}", "${aws_security_group.nat.id}", "${aws_security_group.redis.id}", "${aws_security_group.mysql.id}"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     tags {

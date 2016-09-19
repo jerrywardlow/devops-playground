76fddefdbce8c5516b43c44abc166dabaa8f9c4b
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-19 12:53:16 -0700

    Routing table association for RDS subnets

--- wordpress-cloud/private-subnet.tf
+++ wordpress-cloud/private-subnet.tf
@@ -46,3 +46,13 @@ resource "aws_route_table_association" "private" {
     subnet_id = "${aws_subnet.private.id}"
     route_table_id = "${aws_route_table.private.id}"
 }
+
+resource "aws_route_table_association" "rds1" {
+    subnet_id = "${aws_subnet.rds1.id}"
+    route_table_id = "${aws_route_table.private.id}"
+}
+
+resource "aws_route_table_association" "rds2" {
+    subnet_id = "${aws_subnet.rds2.id}"
+    route_table_id = "${aws_route_table.private.id}"
+}

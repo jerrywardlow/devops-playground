8dd658dc05ef1bbf860e40ef1c0ef7fa1f47c8f9
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-22 15:18:22 -0700

    Accept 80/443 traffic from RDS subnets

--- wordpress-cloud/security-groups.tf
+++ wordpress-cloud/security-groups.tf
@@ -40,14 +40,20 @@ resource "aws_security_group" "nat" {
         from_port = 80
         to_port = 80
         protocol = "tcp"
-        cidr_blocks = ["${var.private_subnet_cidr}"] # Private subnet CIDR
+        cidr_blocks = ["${var.private_subnet_cidr}",
+                       "${var.rds1_subnet_cidr}",
+                       "${var.rds2_subnet_cidr}"
+                      ]
     }
 
     ingress {
         from_port = 443
         to_port = 443
         protocol = "tcp"
-        cidr_blocks = ["${var.private_subnet_cidr}"] # Private subnet CIDR
+        cidr_blocks = ["${var.private_subnet_cidr}",
+                       "${var.rds1_subnet_cidr}",
+                       "${var.rds2_subnet_cidr}"
+                      ]
     }
 
     egress {

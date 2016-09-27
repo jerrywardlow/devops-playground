842d5fbb44a170e0bd4c4ff0ec4b16a26f5f9293
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-27 12:39:17 -0700

    Update cache and install packages

---
+++ wordpress-cloud/packer/scripts/wordpress.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+sudo apt-get install apache2 \
+                     php5 \
+                     php5-cli \
+                     php5-fpm \
+                     php5-gd \
+                     libssh2-php \
+                     libapache2-mod-php5 \
+                     php5-mcrypt \
+                     php5-mysql \
+                     git \
+                     unzip \
+                     zip \
+                     postfix \
+                     php5-curl \
+                     mailutils \
+                     php5-json

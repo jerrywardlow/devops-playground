87c282b7f8e6b94df70cf7bd96262e83b0116eb5
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-10-12 22:03:57 -0700

    Basic html page

--- chef-tutorial/webserver.rb
+++ chef-tutorial/webserver.rb
@@ -9,3 +9,11 @@ service 'apache2' do
   supports status: "true"
   action [:enable, :start]
 end
+
+file '/var/www/html/index.html' do
+  content '<html>
+  <body>
+    <h1>Hello, World!</h1>
+  </body>
+</html>'
+end

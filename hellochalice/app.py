a860e3e28684f6eefc69ca026155285d1d916ab0
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-10-26 12:11:11 -0700

    Add route for '/cities/{city}'

--- hellochalice/app.py
+++ hellochalice/app.py
@@ -2,8 +2,15 @@ from chalice import Chalice
 
 app = Chalice(app_name='hellochalice')
 
+CITIES_TO_STATE = {
+        'seattle': 'WA',
+        'portland': 'OR',
+        }
 
 @app.route('/')
 def index():
     return {'hello': 'world'}
 
+@app.route('/cities/{city}')
+def state_of_city(city):
+    return {'state': CITIES_TO_STATE[city]}

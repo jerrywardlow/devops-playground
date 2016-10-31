760d98abc2a5eee859eb87cdd06de59bcb91aac3
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-10-30 18:02:25 -0700

    Conveniences

--- bastion-test/alter.sh
+++ bastion-test/alter.sh
@@ -2,7 +2,10 @@
 set -e
 
 # Query for bastion IP
+echo "Querying AWS for Bastion public IP address..."
 BASTION_IP=$(aws ec2 describe-instances --filters Name=tag:name,Values=bastion --output text --query 'Reservations[0].Instances[0].PublicIpAddress')
+echo "Bastion IP: ${BASTION_IP}"
 
 # Generate config file with queried IP
 sed "s/HOST_NAME/${BASTION_IP}/g" sample.cfg.template > sample.cfg
+echo "SSH config generated" 

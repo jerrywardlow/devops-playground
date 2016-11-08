e18d14595822058bda58fc96015b157e3ca57089
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-11-08 12:16:16 -0800

    Check environment variables

--- wordpress-cloud/verify.sh
+++ wordpress-cloud/verify.sh
@@ -21,3 +21,16 @@ if ! hash ansible 2>/dev/null; then
   echo "Missing dependency: Ansible"
   exit 1
 fi
+
+# Check environment variables
+
+if [ -z ${AWS_ACCESS_KEY_ID+x} ];
+    echo "AWS_ACCESS_KEY_ID is unset"
+    exit 1
+fi
+
+if [ -z ${AWS_SECRET_ACCESS_KEY+x} ];
+    echo "AWS_SECRET_ACCESS_KEY is unset"
+    exit 1
+fi
+

5deda0182de91e916870f83202521426be3f9a2d
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-11-08 12:31:50 -0800

    Remove exits

--- wordpress-cloud/verify.sh
+++ wordpress-cloud/verify.sh
@@ -9,28 +9,23 @@ set -e
 
 if ! hash packer 2>/dev/null; then
   echo "Missing dependency: Packer"
-  exit 1
 fi
 
 if ! hash terraform 2>/dev/null; then
   echo "Missing dependency: Terraform"
-  exit 1
 fi
 
 if ! hash ansible 2>/dev/null; then
   echo "Missing dependency: Ansible"
-  exit 1
 fi
 
 # Check environment variables
 
 if [ -z ${AWS_ACCESS_KEY_ID+x} ]; then
     echo "AWS_ACCESS_KEY_ID is unset"
-    exit 1
 fi
 
 if [ -z ${AWS_SECRET_ACCESS_KEY+x} ]; then
     echo "AWS_SECRET_ACCESS_KEY is unset"
-    exit 1
 fi
 

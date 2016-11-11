ce36fe93243936580f10fb8452c6d8be9f630b0b
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-11-10 17:52:05 -0800

    Update region and CentOS AMI variables

--- salt-consul/variables.tf
+++ salt-consul/variables.tf
@@ -1,4 +1,9 @@
-variable "ssh_id" {
-    description = "SSH key ID"
-    default = "1652817"
+variable "region" {
+    description = "AWS region"
+    default = "us-west-2"
+}
+
+variable "centos-ami" {
+    description = "CentOS 7 x64 v1602 AMI for us-west-2"
+    default = "ami-d2c924b2"
 }

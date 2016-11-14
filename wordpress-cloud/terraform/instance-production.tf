d340381e72e1b791eabcad42ec6c9b8c30ea6ecd
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-11-13 17:39:24 -0800

    Increase count to match available AZs

--- wordpress-cloud/terraform/instance-production.tf
+++ wordpress-cloud/terraform/instance-production.tf
@@ -1,9 +1,9 @@
 # Production web servers
 resource "aws_instance" "web-production" {
-    count = 2
+    count = 3
     ami = "${var.ubuntu-ami}"
     instance_type = "t2.micro"
-    subnet_id = "${aws_subnet.private.id}"
+    subnet_id = "${element(aws_subnet.private.*.id, count.index)}"
     vpc_security_group_ids = ["${aws_security_group.web_production.id}"]
     key_name = "${aws_key_pair.wordpress.key_name}"
     tags = {

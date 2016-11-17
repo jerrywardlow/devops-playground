a8e73570e5d14603c245e871d576a66dd42b8fef
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-11-16 21:51:43 -0800

    Use default AWS tag

--- bastion-test/alter.sh
+++ bastion-test/alter.sh
@@ -3,7 +3,7 @@ set -e
 
 # Query for bastion IP
 echo "Querying AWS for Bastion public IP address..."
-BASTION_IP=$(aws ec2 describe-instances --filters Name=tag:name,Values=bastion --output text --query 'Reservations[0].Instances[0].PublicIpAddress')
+BASTION_IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=bastion --output text --query 'Reservations[0].Instances[0].PublicIpAddress')
 echo "Bastion IP: ${BASTION_IP}"
 
 # Generate config file with queried IP

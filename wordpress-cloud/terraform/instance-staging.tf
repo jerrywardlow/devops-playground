535deca1cfd69a8e7ea12db9c2f9798ef6d59f4b
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-11-18 16:23:34 -0800

    Use user-data template for staging server

--- wordpress-cloud/terraform/instance-staging.tf
+++ wordpress-cloud/terraform/instance-staging.tf
@@ -5,6 +5,9 @@ resource "aws_instance" "web-staging" {
     subnet_id = "${aws_subnet.public.id}"
     vpc_security_group_ids = ["${aws_security_group.web_staging.id}"]
     key_name = "${aws_key_pair.wordpress.key_name}"
+
+    user_data = "${data.template_file.user_data.rendered}"
+
     tags = {
         Name = "wordpress-web-staging"
         group = "staging"

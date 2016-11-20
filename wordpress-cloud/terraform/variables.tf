df1fd26e74400c5a24cf232c683ceab789db1925
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-11-20 11:46:09 -0800

    Remove default Ubuntu AMI

--- wordpress-cloud/terraform/variables.tf
+++ wordpress-cloud/terraform/variables.tf
@@ -12,11 +12,6 @@ variable "region" {
     default = "us-west-2"
 }
 
-variable "ubuntu-ami" {
-    description = "Ubuntu 14.04 AMI for us-west-2"
-    default = "ami-d732f0b7"
-}
-
 variable "nat-ami" {
     description = "Amazon Linux AMI VPC NAT x86_64 HVM GP2"
     default = "ami-69ae8259"

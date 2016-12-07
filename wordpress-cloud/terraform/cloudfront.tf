ac7352181e419f6b80496e0779cdc9b94b27d42e
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-12-06 20:52:45 -0800

    Add default cache behavior

--- wordpress-cloud/terraform/cloudfront.tf
+++ wordpress-cloud/terraform/cloudfront.tf
@@ -1,7 +1,22 @@
 # CloudFront CDN Distribution
 resource "aws_cloudfront_distribution" "wordpress_cdn" {
+    enabled = true
+
     origin {
         domain_name = "${aws_s3_bucket.wordpress.id}.s3.amazonaws.com"
         origin_id = "S3-${aws_s3_bucket.wordpress.id}"
+    }
+
+    default_cache_behavior {
+        allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+        cached_methods   = ["GET", "HEAD"]
+        target_origin_id = "S3-${aws_s3_bucket.wordpress.id}"
+
+        forwarded_values {
+            query_string = true
+            cookies {
+                forward = "none"
+            }
+        }
     }
 }

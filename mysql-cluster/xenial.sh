9ce740b9188b570eca3fa1613102591cc51368b2
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-12-09 18:58:11 -0800

    Set cluster master config

--- mysql-cluster/xenial.sh
+++ mysql-cluster/xenial.sh
@@ -6,3 +6,23 @@ sudo apt install -y \
 
 wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.5/mysql-cluster-gpl-7.5.4-debian8-x86_64.deb
 sudo dpkg -i mysql-cluster*.deb
+
+sudo mkdir -p /var/lib/mysql-cluster
+
+cat > /var/lib/mysql-cluster << EOF
+[ndb_mgmd]
+hostname=master.sql.cluster
+datadir=/var/lib/mysql-cluster
+
+[ndbd]
+hostname=node1.sql.cluster
+datadir=/usr/local/mysql/data
+
+[ndbd]
+hostname=node2.sql.cluster
+datadir=/usr/local/mysql/data
+
+[mysqld]
+hostname=master.sql.cluster
+EOF
+

b38239be17e9de6f5a5508a8c95edc46cef42eba
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-12-31 11:36:38 -0800

    Add security group

--- hello-ricky/main.tf
+++ hello-ricky/main.tf
@@ -2,6 +2,30 @@ provider "aws" {
     region = "us-west-2"
 }
 
+resource "aws_security_group" "main" {
+    name = "instance"
+    description = "Insecure security group for 'hello-ricky'"
+    
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags {
+        Name = "instance"
+        group = "hello-ricky"
+    }
+}
+
 resource "aws_instance" "web" {
     ami = "ami-b7a114d7"
     instance_type = "t2.micro"

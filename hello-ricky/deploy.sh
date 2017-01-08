b9fb46131bc795704a8c8cd92922b0cdf70ee91d
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-01-07 20:35:45 -0800

    Formatting

--- hello-ricky/deploy.sh
+++ hello-ricky/deploy.sh
@@ -3,9 +3,11 @@
 # Shell script to initialize project infrastructure and provide user with
 # additional feedback
 
+
 set -e # Ditch out if there is a problem executing a command
 
 set -u # Exit if an unset variable is referenced
+
 
 # Check to see that Terraform is installed
 if ! hash terraform 2>/dev/null; then
@@ -13,16 +15,29 @@ if ! hash terraform 2>/dev/null; then
     exit 1
 fi
 
+
 # Make sure AWS specific environment variables are set
 : "${AWS_ACCESS_KEY_ID:?Need to set AWS Access Key}"
 : "${AWS_SECRET_ACCESS_KEY:?Need to set AWS Secret Key}"
 
+
 # Run Terraform to generate the infrastructure
 terraform apply
 
+
 # Query AWS for some information
-WEB_IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=web Name=instance-state-name,Values=running --output text --query 'Reservations[0].Instances[0].PublicIpAddress')
-DB_IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=db Name=instance-state-name,Values=running --output text --query 'Reservations[0].Instances[0].PublicIpAddress')
+WEB_IP=$(aws ec2 describe-instances \
+             --filters Name=tag:Name,Values=web \
+                       Name=instance-state-name,Values=running \
+             --output text \
+             --query 'Reservations[0].Instances[0].PublicIpAddress')
+
+DB_IP=$(aws ec2 describe-instances \
+            --filters Name=tag:Name,Values=db \
+                      Name=instance-state-name,Values=running \
+            --output text \
+            --query 'Reservations[0].Instances[0].PublicIpAddress')
+
 
 # Provide the user with some information about the environment
 echo $'\n\nWeb and database servers accessible via SSH.\n'

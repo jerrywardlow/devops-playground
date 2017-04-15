71e9cd86250ce7201d2b0b4fc39342ee187adee2
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-15 08:44:22 -0700

    Spelling is hard

--- lambda-excuse/bot.js
+++ lambda-excuse/bot.js
@@ -1,7 +1,7 @@
 var bot = require('claudia-bot-builder'),
     excuse = require('huh');
 
-module.exports = bot(funciton (req) {
+module.exports = bot(function (req) {
     return "We've recieved your message: " + req.text +
         ". Your concern is important to us, however " +
         excuse.get();

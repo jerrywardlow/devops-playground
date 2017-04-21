91ca9bf0a0362fb49ec524310de90889cf915f9c
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-21 14:43:48 -0700

    Daemonize tty.js

--- ttyjs/ttyjs.sh
+++ ttyjs/ttyjs.sh
@@ -6,4 +6,6 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 
 sudo apt-get install -y nodejs node-gyp
 
-sudo npm install -g tty.js
+sudo npm install -g tty.js pm2
+
+pm2 start tty.js

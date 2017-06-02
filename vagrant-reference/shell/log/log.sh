4f6fb1efcbc292e17502472e6b27c6d147e18ebb
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-06-02 13:25:24 -0700

    Add elasticsearch key and repo

--- vagrant-reference/shell/log/log.sh
+++ vagrant-reference/shell/log/log.sh
@@ -1,1 +1,11 @@
 yum install java-1.8.0-openjdk
+
+rpm --import http://packages.elastic.co/GPG-KEY-elasticsearch
+
+echo '[elasticsearch-5.x]
+name=Elasticsearch 5.x repo
+baseurl=http://packages.elastic.co/elasticsearch/5.x/centos
+gpgcheck=1
+gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+' >> /etc/yum.repos.d/elasticsearch.repo

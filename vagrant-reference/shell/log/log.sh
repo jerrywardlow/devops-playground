2bbae32c28dc8c196796a6e5eb26e9f19af3a464
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-06-02 13:53:42 -0700

    Update key/repo address

--- vagrant-reference/shell/log/log.sh
+++ vagrant-reference/shell/log/log.sh
@@ -1,12 +1,13 @@
 yum install java-1.8.0-openjdk
 
-rpm --import http://packages.elastic.co/GPG-KEY-elasticsearch
+# Elasticsearch
+rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 
 echo '[elasticsearch-5.x]
 name=Elasticsearch 5.x repo
-baseurl=http://packages.elastic.co/elasticsearch/5.x/centos
+baseurl=https://artifacts.elastic.co/packages/5.x/yum
 gpgcheck=1
-gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
 enabled=1
 ' >> /etc/yum.repos.d/elasticsearch.repo
 

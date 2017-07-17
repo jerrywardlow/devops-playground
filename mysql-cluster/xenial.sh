830e26ebf4bcd46f4ae70a735551c69caaef9a15
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-07-16 21:12:44 -0700

    Version bump

--- mysql-cluster/xenial.sh
+++ mysql-cluster/xenial.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt install -y \
     libaio1
 
-wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.5/mysql-cluster-gpl-7.5.4-debian8-x86_64.deb
+wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.5/mysql-cluster-gpl-7.5.6-debian8-x86_64.deb
 sudo dpkg -i mysql-cluster*.deb
 
 sudo mkdir -p /var/lib/mysql-cluster

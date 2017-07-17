9b560af8a3b14d8781130a0e388b550a05fa143a
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-07-16 21:08:45 -0700

    Version bump

--- mysql-cluster/centos7.sh
+++ mysql-cluster/centos7.sh
@@ -8,5 +8,5 @@ yum -y install \
 yum -y remove \
     mariadb-libs
 
-wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.5/mysql-cluster-gpl-7.5.4-1.el7.x86_64.rpm-bundle.tar
+wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.5/mysql-cluster-gpl-7.5.6-1.el7.x86_64.rpm-bundle.tar
 tar -xvf mysql-cluster*.tar

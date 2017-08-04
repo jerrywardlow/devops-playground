e51baf380ace3b526b666b9cb19da6bb3140be2c
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-08-04 10:46:20 -0700

    Condense

--- terraform-digitalocean/droplet-web.tf
+++ terraform-digitalocean/droplet-web.tf
@@ -1,29 +1,7 @@
-resource "digitalocean_droplet" "web-1" {
+resource "digitalocean_droplet" "web" {
+    count = 2
     image = "ubuntu-16-04-x64"
-    name = "web-1"
-    region = "sfo1"
-    size = "512mb"
-    private_networking = true
-    ssh_keys = ["${var.ssh_id}"]
-
-    connection {
-        user = "root"
-        type = "ssh"
-        private_key = "${file("~/.ssh/id_rsa")}"
-        timeout = "2m"
-    }
-
-    provisioner "remote-exec" {
-        inline = [
-            "sudo apt update",
-            "sudo apt install -y nginx"
-        ]
-    }
-}
-
-resource "digitalocean_droplet" "web-2" {
-    image = "ubuntu-16-04-x64"
-    name = "web-2"
+    name = "web-${count.index}"
     region = "sfo1"
     size = "512mb"
     private_networking = true

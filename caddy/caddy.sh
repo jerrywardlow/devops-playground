3cf723fc727e1dfbc588f1dc8ce46030edd088d9
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-08-05 21:07:30 -0700

    Dir/file prep

--- caddy/caddy.sh
+++ caddy/caddy.sh
@@ -4,3 +4,11 @@ apt update
 apt install -y htop strace net-tools curl
 
 curl -s https://getcaddy.com | bash
+
+mkdir -p /etc/caddy/
+chown root:www-data /etc/caddy
+
+cp /vagrant/Caddyfile /etc/caddy/
+
+mkdir -p /var/www/caddy/
+echo "Become the Mazda Miata you want to see in the world" >> /var/www/caddy/index.html

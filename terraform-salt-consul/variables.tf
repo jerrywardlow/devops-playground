24f6a78c286a0b25457d853f481b161096ec873d
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-08-12 16:24:47 -0700

    Remove unused var

--- terraform-salt-consul/variables.tf
+++ terraform-salt-consul/variables.tf
@@ -3,11 +3,6 @@ variable "region" {
     default = "us-west-2"
 }
 
-variable "centos-ami" {
-    description = "CentOS 7 x64 v1602 AMI for us-west-2"
-    default = "ami-d2c924b2"
-}
-
 data "aws_ami" "centos-ami" {
     most_recent = true
     filter {

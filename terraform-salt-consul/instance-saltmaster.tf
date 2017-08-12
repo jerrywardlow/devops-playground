c92ca2f4331f87a20bf99a7a625e7017511e4399
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-08-12 16:24:31 -0700

    Call AMI data source

--- terraform-salt-consul/instance-saltmaster.tf
+++ terraform-salt-consul/instance-saltmaster.tf
@@ -1,6 +1,6 @@
 # Salt master
 resource "aws_instance" "master" {
-    ami = "${var.centos-ami}"
+    ami = "${data.aws_ami.centos-ami}"
     instance_type = "t2.micro"
     subnet_id = "pass"
     vpc_security_group_ids = ["${aws_security_group.salt_master.id}"]

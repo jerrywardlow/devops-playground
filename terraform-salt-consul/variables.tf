cbde6449460733b8a2fd462176a651c0db3fc990
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-08-12 16:24:19 -0700

    Data source for CentOS 7 AMI

--- terraform-salt-consul/variables.tf
+++ terraform-salt-consul/variables.tf
@@ -7,3 +7,12 @@ variable "centos-ami" {
     description = "CentOS 7 x64 v1602 AMI for us-west-2"
     default = "ami-d2c924b2"
 }
+
+data "aws_ami" "centos-ami" {
+    most_recent = true
+    filter {
+        name = "name"
+        values = ["CentOS Linux 7 x86_64 HVM EBS*"]
+    }
+    owners = ["679593333241"]
+}

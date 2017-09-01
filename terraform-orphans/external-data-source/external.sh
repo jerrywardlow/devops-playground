cca2548a24557eed3b5c10dd6a9be8e3a1a352c0
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-08-31 18:15:13 -0700

    Evaluate injected query and return data

--- terraform-orphans/external-data-source/external.sh
+++ terraform-orphans/external-data-source/external.sh
@@ -2,4 +2,7 @@
 
 WAPPLE_GLOBAL="wapple-string"
 
-jq -n --arg wapple_minor "$WAPPLE_GLOBAL" '{"wapple":$wapple_minor}'
+# Pull data from Terraform query injection
+eval "$(jq -r '@sh "EXAMPLE=\(.example)"')"
+
+jq -n --arg wapple_minor "$WAPPLE_GLOBAL" --arg example "$EXAMPLE"'{"wapple":$wapple_minor, "example":$example}'

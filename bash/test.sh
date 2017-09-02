b7bff36d83cc00bda97af1cf107513ccf11d2f2a
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-09-02 12:30:29 -0700

    Query robots.txt for sitemap locations

--- bash/test.sh
+++ bash/test.sh
@@ -6,3 +6,17 @@ sitemap_crawler() {
         time curl --write-out "\n%{url_effective} -> %{http_code}" --user-agent 'Robo Warmer' --silent --location "$link" --output /dev/null
     done
 }
+
+robo_reader() {
+    wget -q $1 --no-cache --output-document - | \
+    grep -i "sitemap" | \
+    egrep -o "https?://.*" | \
+    while read -r sitemap; do
+		sitemap_log="/tmp/robo_warmer.$(echo $sitemap | shasum | cut -f1 -d " ").log"
+		echo "Logging $sitemap into $sitemap_log"	
+        sitemap_crawler $sitemap > $sitemap_log 2>&1 &
+    done
+
+	echo "Robo warmer is doing the only thing he knows how to do!"
+}
+

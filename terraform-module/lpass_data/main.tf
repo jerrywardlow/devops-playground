24a28eaf62b144df8ac27a0e2f96018dfa1a40bd
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-09-21 13:29:10 -0700

    Remove github and chef key temporarily

--- terraform-module/lpass_data/main.tf
+++ terraform-module/lpass_data/main.tf
@@ -5,11 +5,3 @@ data "external" "lpass" {
 output "lpass_do_token" {
     value = "${data.external.lpass.result.do_token}"
 }
-
-output "lpass_github_key" {
-    value = "${data.external.lpass.result.github_key}"
-}
-
-output "lpass_chef_secret_key" {
-    value = "${data.external.lpass.result.chef_secret_key}"
-}

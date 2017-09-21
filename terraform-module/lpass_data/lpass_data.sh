2f2437d5945e16022b9026308c9867ea9e62222f
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-09-21 13:29:49 -0700

    LastPass CLI scraper wrapper

---
+++ terraform-module/lpass_data/lpass_data.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+eval "$(jq -r '@sh "PROJECT=\(.project)"')"
+
+# Check if logged in to LastPass
+if ! lpass status | grep -q "Logged in as " ; then
+    echo " " >&2 # HACK: Terraform won't interpret \n
+    echo " " >&2
+    echo "Please login to LastPass CLI" >&2
+    echo "EXAMPLE" >&2
+    echo "    $ lpass login username@example.com" >&2
+    exit 1
+fi
+
+DO_TOKEN=$(lpass show "$PROJECT/DigitalOcean API Token - Terraform" --notes)
+
+jq -n --arg do_token "$DO_TOKEN" '{"do_token":$do_token}'

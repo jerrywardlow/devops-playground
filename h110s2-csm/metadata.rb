43bcfd6b8746dcba50c19300e09430a6390e795e
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-09-24 19:53:11 -0700

    Metadata

---
+++ h110s2-csm/metadata.rb
@@ -0,0 +1,12 @@
+name 'joty_h110s2csm'
+maintainer 'Jerry Wardlow'
+maintainer_email 'jerry@jerryoftheyear.com'
+license 'GPLv3'
+description 'Maintains H110S2-CSM Server'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+
+source_url 'https://github.com/jerrywardlow/devops-playground/h110s2-csm'
+issues_url 'https://en.wikipedia.org/wiki/Null_device'
+
+supports 'ubuntu', '16.04'

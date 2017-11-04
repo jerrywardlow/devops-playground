30d8a035a2e39ac18ab3ce058f616a6035f18151
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-04 16:13:42 -0700

    Specify final snapshot

--- terraform-wordpress-cloud/terraform/rds-mysql.tf
+++ terraform-wordpress-cloud/terraform/rds-mysql.tf
@@ -13,6 +13,8 @@ resource "aws_db_instance" "mysql" {
 
     vpc_security_group_ids = ["${aws_security_group.mysql.id}"]
     db_subnet_group_name = "${aws_db_subnet_group.default.name}"
+
+    skip_final_snapshot = true
     
     tags {
       group = "twc-rds"

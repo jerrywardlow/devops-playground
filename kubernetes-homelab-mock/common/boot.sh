a1092c142075bf0d100361be2ff4a4b8aca16b85
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-14 18:28:16 -0800

    Pull etcd container image

--- kubernetes-homelab-mock/common/boot.sh
+++ kubernetes-homelab-mock/common/boot.sh
@@ -9,3 +9,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 apt-get update
 apt-get install docker-ce
+
+# etcd
+docker pull rancher/etcd:v3.2.9-1

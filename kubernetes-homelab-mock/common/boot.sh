1536ec17f92f26d2a97cce5ad58d4ce5870eab77
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-14 18:22:04 -0800

    Install Docker-CE

--- kubernetes-homelab-mock/common/boot.sh
+++ kubernetes-homelab-mock/common/boot.sh
@@ -3,3 +3,9 @@
 apt-get update
 
 apt-get install -y python-minimal
+
+# Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+apt-get update
+apt-get install docker-ce

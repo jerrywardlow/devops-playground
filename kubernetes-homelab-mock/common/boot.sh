35235a54e8cae54a288fcfb2cec2cb82d05e4011
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-16 12:25:26 -0800

    Skip etcd container for now

--- kubernetes-homelab-mock/common/boot.sh
+++ kubernetes-homelab-mock/common/boot.sh
@@ -5,6 +5,3 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 apt-get update
 apt-get install -y docker-ce
-
-# etcd
-docker pull rancher/etcd:v3.2.9-1

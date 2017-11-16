31cc96b9481295b9b4f9b83572d30fd06a4bdcb0
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-16 12:09:32 -0800

    Remove python-minimal, skip Ansible stuff for now

--- kubernetes-homelab-mock/common/boot.sh
+++ kubernetes-homelab-mock/common/boot.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env sh
 
-apt-get update
-
-apt-get install -y python-minimal
-
 # Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"

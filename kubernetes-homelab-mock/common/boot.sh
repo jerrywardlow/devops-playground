9008cb7b690eabf97e5a38e0fb21dca7729b2d15
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-17 16:00:20 -0800

    Add 'ubuntu' to 'docker' group

--- kubernetes-homelab-mock/common/boot.sh
+++ kubernetes-homelab-mock/common/boot.sh
@@ -5,3 +5,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 apt-get update
 apt-get install -y docker-ce
+
+usermod -a -G docker ubuntu

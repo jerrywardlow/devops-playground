e0cb8f89c2a87d35e2e8ac8f43cacbd6b1c4973a
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-17 16:31:14 -0800

    Move to common

--- kubernetes-homelab-mock/common/boot.sh
+++ kubernetes-homelab-mock/common/boot.sh
@@ -7,3 +7,9 @@ apt-get update
 apt-get install -y docker-ce
 
 usermod -a -G docker ubuntu
+
+# K8s
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo add-apt-repository "deb https://apt.kubernetes.io/ kubernetes-xenial main"
+apt-get update
+apt-get install -y kubeadm
--- kubernetes-homelab-mock/nodes/boot.sh
+++ kubernetes-homelab-mock/nodes/boot.sh
@@ -1,7 +1,1 @@
 #!/usr/bin/env sh
-
-# K8s
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo add-apt-repository "deb https://apt.kubernetes.io/ kubernetes-xenial main"
-apt-get update
-apt-get install -y kubeadm

8174a660732b66a7e9ecdabe32095bc0bc1d987b
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-18 19:06:17 -0800

    Lean on Rancher to do the k8s work

--- kubernetes-homelab-mock/master/boot.sh
+++ kubernetes-homelab-mock/master/boot.sh
@@ -3,3 +3,9 @@
 docker pull rancher/server:preview
 
 docker run -d --restart=unless-stopped -p 80:8080 rancher/server:preview
+
+# K8s
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo add-apt-repository "deb https://apt.kubernetes.io/ kubernetes-xenial main"
+apt-get update
+apt-get install -y kubeadm

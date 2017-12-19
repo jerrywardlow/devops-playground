bf3fcd7492b7e718bf90d5dd2c1d56ec718e94c3
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-12-19 10:04:32 -0800

    LB script

---
+++ keepalived-apache/shell/balancer.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Apache shenanigans
+apt update && apt install -y apache2
+
+a2dissite 000-default.conf
+
+cp /tmp/site.conf /etc/apache2/sites-available/
+a2ensite site.conf
+
+a2enmod proxy proxy_http proxy_balancer lbmethod_byrequests
+
+
+# Keepalived shenanigans
+apt install -y keepalived
+
+cp /tmp/keepalived.conf /etc/keepalived/
+
+systemctl start keepalived

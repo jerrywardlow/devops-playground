8e8b3ca8285d972ef52213506ceb956e88081ff5
Jerry Wardlow <jerry@jerryoftheyear.com>
2018-02-16 10:55:56 -0800

    Delete

--- seo-injector/csv.php
+++
@@ -1,8 +0,0 @@
-<?php if ( isset( $results['rows'] ) && count( $results['rows'] ) ): ?>
-	<h3>CSV</h3>
-	<textarea style='width:100%;overflow:auto;padding:2px;'><?php
-		foreach ( $results['rows'] as $row ) {
-			print SEO_Injector_Plugin::str_putcsv($row) . "\n";
-		}
-	?></textarea>
-<?php endif; ?>

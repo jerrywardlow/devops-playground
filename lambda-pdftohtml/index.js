d79e565c1ff643c294aab16a417905c1d88f06b5
Jerry Wardlow <jerry@jerryoftheyear.com>
2018-02-20 15:11:23 -0800

    Update PATH with Lambda task root

--- lambda-pdftohtml/index.js
+++ lambda-pdftohtml/index.js
@@ -1,3 +1,5 @@
 var AWS = require('aws-sdk');
 var memorystream = require('memorystream');
 var wkhtmltopdf = require('wkhtmltopdf');
+
+process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];

557a2676e0a871f53e65e5ae29b247f9a2a9e011
Jerry Wardlow <jerry@jerryoftheyear.com>
2018-02-20 15:20:45 -0800

    New handler

--- lambda-pdftohtml/index.js
+++ lambda-pdftohtml/index.js
@@ -3,3 +3,8 @@ var memorystream = require('memorystream');
 var wkhtmltopdf = require('wkhtmltopdf');
 
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
+
+exports.handler = function(event, context) {
+    var mem_stream = new memorystream();
+    var html_payload = new Buffer(event.html_base64, 'base64').toString('utf8');
+}

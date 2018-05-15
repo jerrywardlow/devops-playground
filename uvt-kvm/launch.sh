583246a5fb0ba9ac2d15e126d7c99e5faba79abc
Jerry Wardlow <jerry@jerryoftheyear.com>
2018-05-14 17:24:25 -0700

    Simplify, bump to bionic

--- uvt-kvm/launch.sh
+++ uvt-kvm/launch.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
 # Seed user-data with hostname
-sed "s/%HOSTNAME%/$1/" user_data > user_data_$1
+sed "s/%HOSTNAME%/$1/" user_data > /tmp/user_data_$1
 
 sudo uvt-kvm create \
     --memory 1024 \
     --cpu 1 \
     --disk 10 \
     --bridge mainbr0 \
-    --user-data user_data_$1 \
+    --user-data /tmp/user_data_$1 \
     $1 \
-    release=xenial
-
-mv user_data_$1 /tmp/
+    release=bionic
